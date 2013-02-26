a7cec69c2ce7081f8b336dd5b204b0fef36dcf38
Entroper <entroper@gmail.com>
2013-02-26 01:23:05 -0500

    first crack at GameController

---
+++ EloSite/Models/GameViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EloDataModel;
+
+namespace EloSite.Models
+{
+	public class GameViewModel
+	{
+		public List<GameScore> Scores { get; set; }
+	}
+}
